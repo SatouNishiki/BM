dd5fc099d728b47941602929b06c4eec97e2813f
karin_0624 <kalin000624@gmail.com>
2015-10-31 13:37:11 +0900

    BoxScore動いた

---
+++ BasketballManagementSystem/bmForm/boxScore/FormBoxScorePresenter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasketballManagementSystem.abstracts;
+using System.Windows.Forms;
+
+namespace BasketballManagementSystem.bmForm.boxScore
+{
+    public class FormBoxScorePresenter : AbstractPresenter
+    {
+        private FormBoxScoreModel boxScoreModel;
+        private FormBoxScoreView boxScoreView;
+        private Form form;
+
+        public FormBoxScorePresenter(FormBoxScoreModel model, FormBoxScoreView view)
+            : base(view, model)
+        {
+            this.boxScoreModel = model;
+            this.boxScoreView = view;
+            this.form = view;
+            view.Presenter = this;
+
+            view.Init();
+            
+        }
+
+        public override Form GetForm()
+        {
+            return this.form;
+        }
+
+        public override void ShowView()
+        {
+            this.boxScoreView.Show();
+        }
+    }
+}
---
+++ BasketballManagementSystem/interfaces/actionPointGraph/IActionPointGraphView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasketballManagementSystem.baseClass.player;
+
+namespace BasketballManagementSystem.interfaces.actionPointGraph
+{
+    public interface IActionPointGraphView : IView
+    {
+        event Action<Player> SelectedPlayerChangedEvent;
+
+        void SetMyListBox(List<Player> players);
+        void SetOppentListBox(List<Player> players);
+        void SetMyTeamName(string name);
+        void SetOppentTeamName(string name);
+
+    }
+}
