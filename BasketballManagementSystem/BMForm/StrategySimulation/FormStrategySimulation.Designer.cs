d131c87aef4035fd1842c1aee202303aec7474ef
Graycat130P <graycat.130p@gmaill.com>
2016-02-15 12:24:54 +0900

    チーム戦略なんたらのコート差し替え

--- BasketballManagementSystem/BMForm/StrategySimulation/FormStrategySimulation.Designer.cs
+++ BasketballManagementSystem/BMForm/StrategySimulation/FormStrategySimulation.Designer.cs
@@ -51,65 +51,49 @@
             // 
             // BenchDragDropBox
             // 
+            resources.ApplyResources(this.BenchDragDropBox, "BenchDragDropBox");
             this.BenchDragDropBox.AllowDrop = true;
             this.BenchDragDropBox.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.BenchDragDropBox.Location = new System.Drawing.Point(9, 404);
             this.BenchDragDropBox.LocationBitmapList = ((System.Collections.Generic.List<DragDropPictureBox.LocationBitmap>)(resources.GetObject("BenchDragDropBox.LocationBitmapList")));
             this.BenchDragDropBox.Name = "BenchDragDropBox";
-            this.BenchDragDropBox.Size = new System.Drawing.Size(867, 103);
-            this.BenchDragDropBox.TabIndex = 0;
             this.BenchDragDropBox.TabStop = false;
             // 
             // dragDropBoxCort
             // 
+            resources.ApplyResources(this.dragDropBoxCort, "dragDropBoxCort");
             this.dragDropBoxCort.AllowDrop = true;
-            this.dragDropBoxCort.Image = ((System.Drawing.Image)(resources.GetObject("dragDropBoxCort.Image")));
-            this.dragDropBoxCort.Location = new System.Drawing.Point(222, 45);
+            this.dragDropBoxCort.Image = global::BasketballManagementSystem.Properties.Resources.coat;
             this.dragDropBoxCort.LocationBitmapList = ((System.Collections.Generic.List<DragDropPictureBox.LocationBitmap>)(resources.GetObject("dragDropBoxCort.LocationBitmapList")));
             this.dragDropBoxCort.Name = "dragDropBoxCort";
-            this.dragDropBoxCort.Size = new System.Drawing.Size(654, 353);
-            this.dragDropBoxCort.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.dragDropBoxCort.TabIndex = 1;
             this.dragDropBoxCort.TabStop = false;
             // 
             // BoardListBox
             // 
+            resources.ApplyResources(this.BoardListBox, "BoardListBox");
             this.BoardListBox.FormattingEnabled = true;
-            this.BoardListBox.ItemHeight = 12;
-            this.BoardListBox.Location = new System.Drawing.Point(12, 45);
             this.BoardListBox.Name = "BoardListBox";
-            this.BoardListBox.Size = new System.Drawing.Size(207, 292);
-            this.BoardListBox.TabIndex = 2;
             this.BoardListBox.SelectedIndexChanged += new System.EventHandler(this.BoardListBox_SelectedIndexChanged);
             // 
             // AddBoardButton
             // 
-            this.AddBoardButton.Location = new System.Drawing.Point(12, 354);
+            resources.ApplyResources(this.AddBoardButton, "AddBoardButton");
             this.AddBoardButton.Name = "AddBoardButton";
-            this.AddBoardButton.Size = new System.Drawing.Size(75, 23);
-            this.AddBoardButton.TabIndex = 3;
-            this.AddBoardButton.Text = "BoardAdd";
             this.AddBoardButton.UseVisualStyleBackColor = true;
             this.AddBoardButton.Click += new System.EventHandler(this.AddBoardButton_Click);
             // 
             // Bench2DragDropBox
             // 
+            resources.ApplyResources(this.Bench2DragDropBox, "Bench2DragDropBox");
             this.Bench2DragDropBox.AllowDrop = true;
             this.Bench2DragDropBox.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.Bench2DragDropBox.Location = new System.Drawing.Point(9, 514);
             this.Bench2DragDropBox.LocationBitmapList = ((System.Collections.Generic.List<DragDropPictureBox.LocationBitmap>)(resources.GetObject("Bench2DragDropBox.LocationBitmapList")));
             this.Bench2DragDropBox.Name = "Bench2DragDropBox";
-            this.Bench2DragDropBox.Size = new System.Drawing.Size(867, 71);
-            this.Bench2DragDropBox.TabIndex = 4;
             this.Bench2DragDropBox.TabStop = false;
             // 
             // SimulateButton
             // 
-            this.SimulateButton.Location = new System.Drawing.Point(103, 354);
+            resources.ApplyResources(this.SimulateButton, "SimulateButton");
             this.SimulateButton.Name = "SimulateButton";
-            this.SimulateButton.Size = new System.Drawing.Size(75, 23);
-            this.SimulateButton.TabIndex = 5;
-            this.SimulateButton.Text = "Simulation";
             this.SimulateButton.UseVisualStyleBackColor = true;
             this.SimulateButton.Click += new System.EventHandler(this.SimulateButton_Click);
             // 
@@ -120,63 +104,55 @@
             // 
             // menuStrip1
             // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.FileToolStripMenuItem,
             this.ConfigToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(884, 26);
-            this.menuStrip1.TabIndex = 6;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // FileToolStripMenuItem
             // 
+            resources.ApplyResources(this.FileToolStripMenuItem, "FileToolStripMenuItem");
             this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
-            this.FileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
-            this.FileToolStripMenuItem.Text = "File";
             // 
             // ConfigToolStripMenuItem
             // 
+            resources.ApplyResources(this.ConfigToolStripMenuItem, "ConfigToolStripMenuItem");
             this.ConfigToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.UseLiteFPSModeItem,
             this.SpeedItem});
             this.ConfigToolStripMenuItem.Name = "ConfigToolStripMenuItem";
-            this.ConfigToolStripMenuItem.Size = new System.Drawing.Size(56, 22);
-            this.ConfigToolStripMenuItem.Text = "Config";
             // 
             // UseLiteFPSModeItem
             // 
+            resources.ApplyResources(this.UseLiteFPSModeItem, "UseLiteFPSModeItem");
             this.UseLiteFPSModeItem.Name = "UseLiteFPSModeItem";
-            this.UseLiteFPSModeItem.Size = new System.Drawing.Size(152, 22);
-            this.UseLiteFPSModeItem.Text = "UseLiteFPS";
             this.UseLiteFPSModeItem.Click += new System.EventHandler(this.UseLiteFPSModeItem_Click);
             // 
             // SpeedItem
             // 
+            resources.ApplyResources(this.SpeedItem, "SpeedItem");
             this.SpeedItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.SpeedComboBox});
             this.SpeedItem.Name = "SpeedItem";
-            this.SpeedItem.Size = new System.Drawing.Size(152, 22);
-            this.SpeedItem.Text = "Speed";
             // 
             // SpeedComboBox
             // 
+            resources.ApplyResources(this.SpeedComboBox, "SpeedComboBox");
             this.SpeedComboBox.Items.AddRange(new object[] {
-            "1",
-            "3",
-            "5",
-            "10",
-            "15"});
+            resources.GetString("SpeedComboBox.Items"),
+            resources.GetString("SpeedComboBox.Items1"),
+            resources.GetString("SpeedComboBox.Items2"),
+            resources.GetString("SpeedComboBox.Items3"),
+            resources.GetString("SpeedComboBox.Items4")});
             this.SpeedComboBox.Name = "SpeedComboBox";
-            this.SpeedComboBox.Size = new System.Drawing.Size(121, 26);
             this.SpeedComboBox.SelectedIndexChanged += new System.EventHandler(this.SpeedComboBox_SelectedIndexChanged);
             this.SpeedComboBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SpeedComboBox_KeyPress);
             // 
             // FormStrategySimulation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 597);
             this.Controls.Add(this.SimulateButton);
             this.Controls.Add(this.Bench2DragDropBox);
             this.Controls.Add(this.AddBoardButton);
@@ -186,7 +162,6 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormStrategySimulation";
-            this.Text = "FormStrategySimulation";
             ((System.ComponentModel.ISupportInitialize)(this.BenchDragDropBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dragDropBoxCort)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Bench2DragDropBox)).EndInit();
